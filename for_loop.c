18c8ae2d0511d33e5d26a2e29a2d57fd36a5fd44
ahmadhridoy <badaruddinahmadridoy11@gmail.com>
2023-10-08 00:59:58 +0600

    forloop

--- for_loop.c
+++ for_loop.c
@@ -1,8 +1,38 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int main (){
-    int n = 25;
+    /*int n = 25;
     for(int i=1; i<=n; i++)
     {
         printf("%d\n", i );
-    }
+    }*/
+
+   /**Variants of the for loop
+    * 
+    *  int h=1, r=12;
+    for(int h=1, r=12;h<=r; h++,r--){
+        printf("%d  %d\n" ,h,r);
+    }*/
+
+/* 2 4 6 8.....2n*/
+
+/*int h;
+
+scanf("%d", &h);
+int sum = 0;
+
+printf("i\t\tsum\n");
+
+for (int i=2; i<=2*h; i+=2)
+{   sum+=i;
+
+    printf("%d\t\t%d\n", i, sum);
 }
+
+}*/
+
+ /*Nested for loop*/
+
+ #include<stdio.h>
+ int main(){
+    
+ }
